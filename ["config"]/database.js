47edb39bc50765b9e25659f61bea7bb4e7ccb51e
Kjetil <kjetil.mjos@gmail.com>
2016-03-25 20:50:46 +0100

    Delete database.js

--- ["config"]/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-var config = require('./config.json');
-module.exports = {
-
-    'url' : config.mongodb_url // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
