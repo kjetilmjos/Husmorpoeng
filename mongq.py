4c76a6134ed6ff37069b1726a5210043a7980c2e
Kjetil <kjetil.mjos@gmail.com>
2016-03-28 09:46:29 +0200

    Added files via upload

--- mongq.py
+++ mongq.py
@@ -16,10 +16,15 @@ db = conn.husmorapp
 collection_users = db.users
 query_all_users = collection_users.find()
 
+collection_done_tasks = db.done_tasks
+query_all_done_tasks = collection_done_tasks.find().sort(u'date', -1)
 
 print "------------------------------------------------------------------------"
 print "Listing all usernames: "
 for k in query_all_users:
     print "Navn: " + k[u'name'] + "     " + "Email: " + k[u'email'] + "   " + "Points: " + str(k['points'])
+print "-------------"
+print "Last 5 task added: "
+for s in query_all_done_tasks[0:5]:
+    print s[u'date']
 print "------------------------------------------------------------------------"
-print "Last task added: "
